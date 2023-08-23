5383ac28a17a3c448fadedf661cdacfecc18dd65
Dmitry Ostapov <OstapovD@terralink-global.com>
2023-08-23 11:24:18 +0300

    1

---
+++ src/main/java/com/example/kr_db/KrDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.kr_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kr_db/KrDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kr_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
