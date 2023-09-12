fbd3e272265eb33b68be779e62b5471a021efd21
Dmitry Ostapov <OstapovD@terralink-global.com>
2023-09-12 12:58:22 +0300

    docker-compose

--- db/scripts/001_ddl_create_users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-    users (
-    id SERIAL PRIMARY KEY,
-    name varchar(15),
-    surname varchar(20)
-);
--- db/scripts/002_dml_insert_users.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (name, surname) values ('Ivan', 'Ivanov');
